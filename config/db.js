89fb64978d47fe0ea2b459d59660b17fcb5959e6
tanyasri <tanyasriganesan@gmail.com>
2026-01-31 14:11:03 +0530

    added start script

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
-const mongoose = require('mongoose')
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 module.exports = () => {
-    mongoose.connect("mongodb://127.0.0.1:27017/userDB")
-.then(() => console.log("MongoDB Connected!!!!!"))
+    mongoose.connect(process.env.DB_URL)
+.then(() => console.log("MongoDB Connected"))
 .catch(err => console.log("DB Error:", err))
 }
--- models/usermodel.js
+++ models/usermodel.js
@@ -2,6 +2,15 @@ const mongoose = require('mongoose')
 
 module.exports = () => {
     mongoose.connect("mongodb://127.0.0.1:27017/userDB")
-.then(() => console.log("MongoDB Connected!!!!!"))
+.then(() => console.log("MongoDB Connected"))
 .catch(err => console.log("DB Error:", err))
 }
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, minlength: [8, "Password must be at least 8 characters long"] },
+    role: { type: String, default: "USER" },
+    age: Number,
+    requestQuota: { type: Number, default: 5 }
+})
